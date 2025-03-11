15670e1ffc4e7db3baddf741aeff6899ac7b0c00
Tom Rambeau <Tom.RAMBEAU@etu.uca.fr>
2025-03-11 22:27:35 +0100

    :feature: projectCard clickable

--- portfolio/src/components/ProjectCard.vue
+++ portfolio/src/components/ProjectCard.vue
@@ -11,22 +11,12 @@ defineProps({
 </script>
 
 <template>
-  <div class="project-card">
+  <router-link :to="`/project/${id}`" class="project-card">
     <div class="project-image-container">
       <img :src="image" :alt="title" class="project-image">
       <div class="project-overlay">
         <h3>{{ title }}</h3>
-        <div class="project-links">
-          <router-link :to="`/project/${id}`" class="overlay-btn">
-            <svg class="icon" viewBox="0 0 24 24"><path fill="currentColor" d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z"/></svg>
-          </router-link>
-          <a v-if="github" :href="github" target="_blank" class="overlay-btn">
-            <svg class="icon" viewBox="0 0 24 24"><path fill="currentColor" d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
-          </a>
-          <a v-if="demo" :href="demo" target="_blank" class="overlay-btn">
-            <svg class="icon" viewBox="0 0 24 24"><path fill="currentColor" d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z"/></svg>
-          </a>
-        </div>
+        
       </div>
     </div>
     <div class="project-content">
@@ -38,7 +28,7 @@ defineProps({
         </span>
       </div>
     </div>
-  </div>
+  </router-link>
 </template>
 
 <style scoped>
@@ -48,6 +38,9 @@ defineProps({
   overflow: hidden;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   transition: transform 0.3s ease, box-shadow 0.3s ease;
+  display: block; /* Ajoutez cette ligne pour s'assurer que le lien occupe tout l'espace */
+  text-decoration: none; /* Ajoutez cette ligne pour enlever le soulignement du lien */
+  color: inherit; /* Ajoutez cette ligne pour garder la couleur du texte originale */
 }
 
 .project-card:hover {
