53d240017eedadfee57b3ec0e5de57be50e762e8
Tom Rambeau <Tom.RAMBEAU@etu.uca.fr>
2025-03-15 10:28:42 +0100

    Updates

---
+++ assets/index-C8rioSGa.js
@@ -0,0 +1,1 @@
+const s="/portfolio",e="/images";function r(t){return`${e}${t}`}function n(t){return`${s}${t}`}export{n as a,r as g};
--- assets/index-Kn5tbwQy.js
+++
@@ -1,1 +0,0 @@
-const s="/portfolio",e="/images";function r(t){return`${e}${t}`}function n(t){return`${s}${t}`}export{r as a,n as g};
