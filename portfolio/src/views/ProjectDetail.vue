3b20e52ec65c0907e8c93504c807f9c99eb21134
Tom Rambeau <Tom.RAMBEAU@etu.uca.fr>
2025-03-22 12:52:38 +0100

    modif couleur bouton retour aux projets

--- portfolio/src/views/ProjectDetail.vue
+++ portfolio/src/views/ProjectDetail.vue
@@ -149,9 +149,7 @@ const scrollToGallery = () => {
           </ScrollReveal>
         </div>
       </div>
-    </div>
-
-    <ScrollReveal>
+      <ScrollReveal>
       <div class="back-button-container">
         <router-link to="/projects" class="back-button">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
@@ -162,6 +160,9 @@ const scrollToGallery = () => {
         </router-link>
       </div>
     </ScrollReveal>
+    </div>
+
+
   </div>
 </template>
 
